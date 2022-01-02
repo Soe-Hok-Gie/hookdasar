3b827c6a7d98084edb3061c01f1d9dd4803245eb
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-02 20:43:00 +0700

    membuat lifeCyle

---
+++ src/components/belajaruseffect/Lifecycle.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Lifecycle extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            name:""
+             
+        }
+    }
+    
+    componentDidMount ()
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
