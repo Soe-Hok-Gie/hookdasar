8f48a530f7043950e1375951501f0b64ef295dc6
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-02 20:32:20 +0700

    stelan dasar

---
+++ src/components/belajaruseffect/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h2>perbedaan lifeCycle & hook </h2>
+            </div>
+        )
+    }
+}
