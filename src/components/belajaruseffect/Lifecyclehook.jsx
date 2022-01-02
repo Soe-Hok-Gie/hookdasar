d5937f17bd078cf8ad7998beeceff64977c00d43
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-02 22:55:03 +0700

    membuat componentDidupdate dgn deklarasi State berupa isUpdate

--- src/components/belajaruseffect/Lifecyclehook.jsx
+++ src/components/belajaruseffect/Lifecyclehook.jsx
@@ -2,11 +2,14 @@ import React,{useState,useEffect } from 'react'
 
 const Lifecyclehook = () => {
     const [name, setName] = useState("");
+    const [isUpdate, setIsupdate] = useState(false);
 
+    //componentdidMount
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users/1')
         .then((response) => response.json())
         .then((json) => {
+            console.log ("componentDidmount")
             setName (json.name)
         });
     },[])
